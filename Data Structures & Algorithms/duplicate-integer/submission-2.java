43b11b6b866921beba64d7c65464b0c99c73d386
Janavika <727724EUAI503@skcet.ac.in>
2026-08-25 10:17:37 +0530

    Add: duplicate-integer - submission-2

---
+++ Data Structures & Algorithms/duplicate-integer/submission-2.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        HashSet<Integer>hs=new HashSet<>();
+        int n=nums.length;
+        for(int i=0;i<n;i++){
+          if(hs.contains(nums[i])){
+            return true;
+          }
+          else{
+            hs.add(nums[i]);
+          }
+        }
+        return false;
+    }
+}
